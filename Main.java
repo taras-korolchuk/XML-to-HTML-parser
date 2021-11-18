8e18fcda6588d9574023d9a14613aa5295bb6e83
taras-overmind <72138768+taras-overmind@users.noreply.github.com>
2021-11-18 09:12:52 +0200

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,10 @@
+
+public class Main {
+    public static void main(String[] args)  {
+        try{
+            DOMsearch.convert("K16");
+            System.out.println("\n");
+            SAXsearch.convert("16");
+    }catch(Exception ex){}
+    }
+}
